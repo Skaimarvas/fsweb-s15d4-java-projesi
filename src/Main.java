86fb3db969d63d53d9f04268d42865d9d2c88e2a
skaimarvas <skaimarvas@gmail.com>
2024-01-26 21:14:35 +0300

    Stack Challange tamamlandı.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -7,9 +12,38 @@ public class Main {
         System.out.println(checkForPalindrome("hello"));
         System.out.println(checkForPalindrome("Don't node"));
 
+
+
     }
 
+
     public static boolean checkForPalindrome(String text) {
-        return false;
+
+        System.out.println("Initial Text: " + text);
+        text = text.replaceAll("[.,'?\\-\\s]","").toLowerCase(Locale.ENGLISH);
+
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        char[] chars = text.toCharArray();
+        for(char c: chars){
+            stack.push(c);
+            queue.offer(c);
+        }
+
+        while(!queue.isEmpty()){
+            Character character = queue.remove();
+            System.out.println("Character: " + character);
+            //stact LIFO;
+            //queque FIFO;
+            if(character != stack.pop()){
+                return false;
+            }
+
+        }
+
+
+        return true;
+
     }
 }
